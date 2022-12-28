b4760b7ef918e1da10bdfdfadcfb799b1ab749cf
“antoanparashkevov” <tonkata1505@gmail.com>
2022-12-28 23:35:48 +0200

    remove connection constant

--- src/config/database_config.js
+++ src/config/database_config.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const configDatabase = async (app) => {
     try {
         //process.env.CONNECTION_URL
-      const connection = await mongoose.connect('mongodb://localhost:27017/freelancers', 
+        await mongoose.connect('mongodb://localhost:27017/freelancers', 
           { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('MongoDB connected')
     } catch (error) {
-        console.log('It has an error from MongoDB client')
+        console.log('It has an error from MongoDB client')  
         console.error(error)
         process.exit(1)
     }
