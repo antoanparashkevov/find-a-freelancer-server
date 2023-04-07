51fe0ca6ad0b045e86c0b02f3b302d9d2b9bfc76
“antoanparashkevov” <tonkata1505@gmail.com>
2023-04-07 09:54:01 +0300

    revert changes

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
-const PORT = process.env.PORT | 9000
+const PORT = process.env.PORT | 5000
 
 //Config
 const configExpress = require('./config/express-configuration.js')
